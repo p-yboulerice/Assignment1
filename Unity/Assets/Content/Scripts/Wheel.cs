e2d0584611624aea07b41e6de1777b60393869ef
PY Boulerice <pboulerice@sagosago.com>
2018-05-11 10:01:32 -0400

    Update mesh layers

--- Unity/Assets/Content/Scripts/Wheel.cs
+++ Unity/Assets/Content/Scripts/Wheel.cs
@@ -11,32 +11,54 @@
 	/// </summary>
 	public class Wheel : MonoBehaviour, ISingleTouchObserver {
 
+		#region Properties
 
 		// Visible
 		[SerializeField]
 		private Camera Camera;
 
+        // Defaults
 		private PhysicsMaterial2D defaultPhysicsMaterial;
+		private Vector2 defaultPosition;
 
 		[SerializeField]
 		private float draggingSpeed;
 
 		private Touch Touch;
 
-        // Components
-        private Rigidbody2D rb2d;
+		// Components
+		private Rigidbody2D rb2d;
 
-        private void Awake() {
-            rb2d = GetComponent<Rigidbody2D>();
+		#endregion
+
+        
+		// Unity Defaults
+
+		private void Awake() {
+			rb2d = GetComponent<Rigidbody2D>();
 			defaultPhysicsMaterial = rb2d.sharedMaterial;
-        }
+			defaultPosition = transform.position;
+		}
 
-        private void FixedUpdate() {
-            DefaultDragging();
-        }
+		private void OnEnable() {
 
-        private void DefaultDragging() {
-            if (this.Touch != null) {
+			TouchDispatcher.Instance.Add(this, 0);
+		}
+
+		private void OnDisable() {
+
+			TouchDispatcher.Instance.Remove(this);
+		}
+
+		private void FixedUpdate() {
+			DefaultDragging();
+		}
+
+
+        // Dragging Functionality
+
+		private void DefaultDragging() {
+			if (this.Touch != null) {
 
 				Vector2 currentVelocity;
 				Vector2 currentPosition = transform.position;
@@ -50,21 +72,28 @@
 
 				Vector2 force = rb2d.mass * targetVelocity / Time.fixedDeltaTime;
 
-				rb2d.AddForce(force);            
-            }
-        }
+				rb2d.AddForce(force);
+			}
+		}
 
-        private void OnEnable() {
 
-            TouchDispatcher.Instance.Add(this, 0);
-        }
+        // Out of bounds Functionality
 
-        private void OnDisable() {
+        private void CheckBounds()
+		{
+		}
 
-            TouchDispatcher.Instance.Remove(this);
-        }
+		private void Respawn()
+		{
+			rb2d.velocity = Vector2.zero;
+			rb2d.angularVelocity = 0;
+			transform.position = defaultPosition;
+		}
 
-        public bool OnTouchBegan(Touch touch) {
+
+        // ISingleTouchObserver Implementation
+
+		public bool OnTouchBegan(Touch touch) {
 
 			// Actually check if the touch is within this object
 			RaycastHit2D hit2D;
@@ -73,27 +102,27 @@
 			hit2D = Physics2D.Raycast(ray, Vector3.back);
 
 			if (hit2D.transform == transform) {
-                rb2d.sharedMaterial = null;            
-                this.Touch = touch;
-                return true;
-            }
-			else{
+				rb2d.sharedMaterial = null;
+				this.Touch = touch;
+				return true;
+			}
+			else {
 				return false;
 			}
-        }
+		}
 
-        public void OnTouchMoved(Touch touch) {
+		public void OnTouchMoved(Touch touch) {
 
-        }
+		}
 
-        public void OnTouchEnded(Touch touch) {
-            this.Touch = null;
+		public void OnTouchEnded(Touch touch) {
+			this.Touch = null;
 			rb2d.sharedMaterial = defaultPhysicsMaterial;
-        }
+		}
 
-        public void OnTouchCancelled(Touch touch) {
+		public void OnTouchCancelled(Touch touch) {
 
-        }
+		}
 
 
 	}
